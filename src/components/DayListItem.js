794702d99fd2ac5fefa2525a97ae5797e7b67841
hoshaun <shaun.ho@mail.utoronto.ca>
2023-04-18 12:11:21 -0400

    testing the DayListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -14,7 +14,13 @@ export default function DayListItem(props) {
       onClick={() => props.setDay(props.name)}
     >
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">
+        {formatSpots(props.spots)} remaining
+      </h3>
     </li>
   );
 }
+
+const formatSpots = function(spots) {
+  return `${spots === 0 ? 'no' : spots} ${spots === 1 ? 'spot' : 'spots'}`;
+};
