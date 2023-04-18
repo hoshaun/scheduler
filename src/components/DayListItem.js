72b17d329f641c9f137f7bc50a0e3e6aef92b96f
hoshaun <shaun.ho@mail.utoronto.ca>
2023-04-18 13:34:38 -0400

    adding state to our app

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -13,6 +13,7 @@ export default function DayListItem(props) {
       className={dayClass} 
       onClick={() => props.setDay(props.name)}
     >
+      <h1></h1>
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">
         {formatSpots(props.spots)} remaining
