eb37eb1a097be216b592b8e4930f75b706c6f1ce
hoshaun <shaun.ho@mail.utoronto.ca>
2023-04-19 14:16:23 -0400

    Displaying Appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,8 +1,20 @@
 import React from "react";
 import 'components/Appointment/styles.scss';
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 export default function Appointment(props) {
+  const interview = props.interview;
+
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time} />
+      {interview ? 
+        <Show student={interview.student} interviewer={interview.interviewer} /> 
+        : 
+        <Empty />
+      }
+    </article>
   );
 }
