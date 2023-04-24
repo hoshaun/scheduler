930bd33d0161bcfa23857f4bc8336938f983a462
hoshaun <shaun.ho@mail.utoronto.ca>
2023-04-24 11:48:56 -0400

    Editing Appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -16,6 +16,7 @@ export default function Appointment(props) {
   const SAVING = "SAVING";
   const DELETING = "DELETING";
   const CONFIRM = "CONFIRM";
+  const EDIT = "EDIT";
   const { mode, transition, back } = useVisualMode(props.interview ? SHOW : EMPTY);
   
   const save = function(name, interviewer) {
@@ -51,11 +52,21 @@ export default function Appointment(props) {
           student={props.interview.student} 
           interviewer={props.interview.interviewer} 
           onDelete={() => transition(CONFIRM)}
+          onEdit={() => transition(EDIT)}
         /> 
       )}
       {mode === CREATE && (
         <Form
           interviewers={props.interviewers}
+          onCancel={back}
+          onSave={save}
+        />
+      )}
+      {mode === EDIT && (
+        <Form
+          interviewers={props.interviewers}
+          student={props.interview.student}
+          interviewer={props.interview.interviewer.id} 
           onCancel={back}
           onSave={save}
         />
